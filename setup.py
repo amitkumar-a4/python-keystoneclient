a1b9f5c8657699ac6b6fc91e65f9a8eaf570b40a
Giri Basava <giri.basava@triliodata.com>
2013-10-01 18:43:45 -0700

    Incremental Checkin...

--- setup.py
+++ setup.py
@@ -6,6 +6,63 @@
 # THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
 import setuptools
 
+from workloadmanager.openstack.common import setup as common_setup
+
+requires = common_setup.parse_requirements()
+depend_links = common_setup.parse_dependency_links()
+project = 'workloadmanager'
+
+filters = [
+    "AvailabilityZoneFilter = "
+    "workloadmanager.openstack.common.scheduler.filters."
+    "availability_zone_filter:AvailabilityZoneFilter",
+    "CapabilitiesFilter = "
+    "workloadmanager.openstack.common.scheduler.filters."
+    "capabilities_filter:CapabilitiesFilter",
+    "CapacityFilter = "
+    "workloadmanager.scheduler.filters.capacity_filter:CapacityFilter",
+    "JsonFilter = "
+    "workloadmanager.openstack.common.scheduler.filters.json_filter:JsonFilter",
+    "RetryFilter = "
+    "workloadmanager.scheduler.filters.retry_filter:RetryFilter",
+]
+
+weights = [
+    "CapacityWeigher = workloadmanager.scheduler.weights.capacity:CapacityWeigher",
+]
+
 setuptools.setup(
-    setup_requires=['pbr>=0.5.21,<1.0'],
-    pbr=True)
+    name=project,
+    version= common_setup.get_version(project, '2013.1.3'),
+    description='WorkloadManager',
+    author='TrilioData',
+    author_email='info@triliodata.com',
+    url='http://www.triliodata.com/',
+    classifiers=[
+        'Environment :: TrilioData',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: TrilioData',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    cmdclass=common_setup.get_cmdclass(),
+    packages=setuptools.find_packages(exclude=['bin', 'smoketests']),
+    install_requires=requires,
+    dependency_links=depend_links,
+    entry_points={
+        'workloadmanager.scheduler.filters': filters,
+        'workloadmanager.scheduler.weights': weights,
+    },
+    include_package_data=True,
+    test_suite='nose.collector',
+    setup_requires=['setuptools_git>=0.4'],
+    scripts=['bin/workloadmanager-all',
+             'bin/workloadmanager-api',
+             'bin/raksha-clear-rabbit-queues',
+             'bin/raksha-rootwrap',
+             'bin/raksha-scheduler',
+             'bin/raksha-rpc-zmq-receiver'],
+    py_modules=[])
