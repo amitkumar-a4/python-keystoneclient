cbffe8a641ec6ae8278c058381b830ee75b5d9da
Giri Basava <giri.basava@triliodata.com>
2013-11-12 21:32:43 -0800

    refactoring 4 : Change backup to workload

--- virt/libvirt/__init__.py
+++
@@ -1,8 +0,0 @@
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
-
-# Copyright (c) 2013 TrilioData, Inc.
-# All Rights Reserved.
-
-from workloadmgr.virt.libvirt import driver
-
-LibvirtDriver = driver.LibvirtDriver
--- virt/virtapi.py
+++
@@ -1,9 +0,0 @@
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
-
-# Copyright (c) 2013 TrilioData, Inc.
-# All Rights Reserved.
-
-
-
-class VirtAPI(object):
-
