3023de7eb781487861677f36b1f7cb94cf32d703
Giri Basava <giri.basava@triliodata.com>
2015-02-02 11:56:00 -0800

    set suds cache to none

--- workloadmgr/virt/vmwareapi/vim.py
+++ workloadmgr/virt/vmwareapi/vim.py
@@ -78,6 +78,7 @@ class Vim:
         self.url = Vim.get_soap_url(protocol, host)
         self.client = suds.client.Client(self.wsdl_url, location=self.url,
                                          plugins=[VIMMessagePlugin()])
+	self.client.set_options(cache=None)
         self._service_content = self.RetrieveServiceContent(
                                         "ServiceInstance")
 
