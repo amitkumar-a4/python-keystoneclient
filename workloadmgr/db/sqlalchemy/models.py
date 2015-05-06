e1f1186f5037b958374d01c5291f4cc4df99c542
stack <stack@tvault-vm>
2015-05-06 15:16:30 -0700

    update version to 1.0.105

--- workloadmgr/db/sqlalchemy/models.py
+++ workloadmgr/db/sqlalchemy/models.py
@@ -23,7 +23,7 @@ from workloadmgr.openstack.common import timeutils
 FLAGS = flags.FLAGS
 BASE = declarative_base()
 
-DB_VERSION = '1.0.48'
+DB_VERSION = '1.0.105'
 
 
 class WorkloadsBase(object):
