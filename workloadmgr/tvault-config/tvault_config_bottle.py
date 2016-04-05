e1b3712015dcc0f91bb9fe5c9961b0f6bef41cdc
giri-basava <giri.basava@triliodata.com>
2016-04-04 22:17:34 -0700

    Fix static route function names

--- workloadmgr/tvault-config/tvault_config_bottle.py
+++ workloadmgr/tvault-config/tvault_config_bottle.py
@@ -271,7 +271,7 @@ def send_tvault_contego_install():
     return static_file('tvault-contego-install.sh', root='/opt/stack/contego/install-scripts', mimetype='text/plain', download=True)    
 
 @bottle.route('/tvault-horizon-plugin-install.sh')
-def send_tvault_horizon_panel():
+def send_tvault_horizon_plugin_install():
     return static_file('tvault-horizon-plugin-install.sh', root='/opt/stack/horizon-tvault-plugin/install-scripts', mimetype='text/plain', download=True)    
     
 @bottle.route('/tvault_panel.py')
@@ -279,7 +279,7 @@ def send_tvault_horizon_panel():
     return static_file('tvault_panel.py', root='/opt/stack/horizon-tvault-plugin/install-scripts', mimetype='text/plain', download=True)    
 
 @bottle.route('/tvault_panel_group.py')
-def send_tvault_horizon_panel():
+def send_tvault_horizon_panel_group():
     return static_file('tvault_panel_group.py', root='/opt/stack/horizon-tvault-plugin/install-scripts', mimetype='text/plain', download=True)    
     
 
