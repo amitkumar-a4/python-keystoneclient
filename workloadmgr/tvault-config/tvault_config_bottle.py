35059a659cfc684a3696b3a7d17d640414d84618
Murali Balcha <murali.balcha@triliodata.com>
2016-05-23 23:20:51 +0000

    TVAULT-816

--- workloadmgr/tvault-config/tvault_config_bottle.py
+++ workloadmgr/tvault-config/tvault_config_bottle.py
@@ -2006,7 +2006,7 @@ def configure_service():
                      'project_name = ' + config_data['service_tenant_name'],
                      starts_with=True)
         replace_line('/etc/workloadmgr/workloadmgr.conf', 'trustee_role = ',
-                     'trustee_role = ' + config_data['trustee_role'],
+                     'trustee_role = ' + config_data.get('trustee_role', '_member_'),
                      starts_with=True)
 
         #configure api-paste
