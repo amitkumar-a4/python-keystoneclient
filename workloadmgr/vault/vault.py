d24927b5610113bd14f35426521578301e3d0ba5
root <jignasha.vithalani@triliodata.com>
2016-10-16 23:49:24 +0000

    TVAULT-1178

--- workloadmgr/vault/vault.py
+++ workloadmgr/vault/vault.py
@@ -66,7 +66,7 @@ wlm_vault_opts = [
                default='local',
                help='NFS Export'),
     cfg.StrOpt('vault_storage_nfs_options',
-               default='rw,nofail,auto,nolock',
+               default='nolock',
                help='NFS Options'),
     cfg.StrOpt('vault_storage_das_device',
                default='none',
