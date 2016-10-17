ecf3481844f7080fc4de25c3420c1f641f6ca7ab
TrilioBuild <trilio.build@gmail.com>
2016-10-16 23:50:28 -0700

    Updating version to 2.2.5

--- workloadmgr/db/imports/import_workload_2_2_4.py
+++ workloadmgr/db/imports/import_workload_2_2_4.py
@@ -10,16 +10,8 @@ LOG = logging.getLogger(__name__)
 def import_settings(cntx, new_version, upgrade=True):
     return import_workload_2_2_3.import_settings(cntx, new_version, upgrade)
 
-<<<<<<< HEAD
-def import_workload(cntx, workload_url, new_version, backup_endpoint, upgrade=True):
-    """ Import workload and snapshot records from vault
-        Versions Supported: 2.2.4
-    """
-    return import_workload_2_2_3.import_workload(cntx, workload_url, new_version, backup_endpoint, upgrade)
-=======
 def import_workload(cntx, workload_ids, new_version, upgrade=True):
     """ Import workload and snapshot records from vault
         Versions Supported: 2.2.4
     """
     return import_workload_2_2_3.import_workload(cntx, workload_ids, new_version, upgrade)
->>>>>>> upstream/master
