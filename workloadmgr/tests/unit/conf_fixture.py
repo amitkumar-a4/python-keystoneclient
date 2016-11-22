b496626ca7d1fc0221d242eaf7549dd438bfd592
Murali Balcha <murali.balcha@triliodata.com>
2016-11-22 12:54:21 -0500

    set the default vault_storage_type as nfs in unit tests

--- workloadmgr/tests/unit/conf_fixture.py
+++ workloadmgr/tests/unit/conf_fixture.py
@@ -16,3 +16,4 @@ def set_defaults(conf):
     conf.set_default('connection', 'sqlite://', group='database')
     conf.set_default('sqlite_synchronous', False)
     conf.set_default('policy_file', 'workloadmgr/tests/unit/policy.json')
+    conf.set_default('vault_storage_type', 'nfs')
