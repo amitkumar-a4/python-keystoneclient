c2a92fed5479ef8d0f50e78881518504f9cd2af7
TrilioBuild <trilio.build@gmail.com>
2016-12-15 04:58:08 -0800

    Revert "Updating version to 2.3.23"
    This reverts commit 4d00a27e0f02c19fa6324e0541ab50735668e3ea.

--- workloadmgr/db/sqlalchemy/models.py
+++ workloadmgr/db/sqlalchemy/models.py
@@ -24,7 +24,7 @@ from workloadmgr.vault import vault
 FLAGS = flags.FLAGS
 BASE = declarative_base()
 
-DB_VERSION = '2.3.23'
+DB_VERSION = '2.3.22'
 
 class WorkloadsBase(object):
     """Base class for Workloads Models."""
