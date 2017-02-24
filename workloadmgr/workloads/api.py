ae7fffcbca60e47d5fcc6fd80d01b1a9e367f36c
jignahsha-vithalani <jignahsha.vithalani@triliodata.com>
2017-02-24 16:39:15 +0000

    Tvault-1453

--- workloadmgr/workloads/api.py
+++ workloadmgr/workloads/api.py
@@ -1750,11 +1750,9 @@ class API(base.Base):
                 error_msg = "Recovery manager instance cannot have ephemeral disk"
                 raise Exception(error_msg)
 
-            try:
-                server_id = server.image['id']
-            except Exception as ex:
-                   error_msg = "Not able to acces VM image, Recovery manager should be booted from Image"
-                   raise Exception(error_msg)
+            if not hasattr(server, 'image'):
+               error_msg = "Not able to acces VM image, Recovery manager should be booted from Image"
+               raise Exception(error_msg) 
 
             (image_service, image_id) = glance.get_remote_image_service(context, server.image['id'])
             metadata = image_service.show(context, server.image['id'])
