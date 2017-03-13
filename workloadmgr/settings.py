fa3c177d4c2e5825c761db3f683b12c89abe4bf2
jignasha-vithalani <jignasha.vithalani@triliodata.com>
2017-03-13 00:29:06 +0000

    Tvault-830

--- workloadmgr/settings.py
+++ workloadmgr/settings.py
@@ -34,18 +34,21 @@ default_settings = {
 def get_settings(context=None, get_hidden=False):                           
     """get settings"""
     try:
+        copy_settings = {}
+        if context.is_admin is True:
+           copy_settings = default_settings
         persisted_settings = {}
         persisted_setting_objs = db.setting_get_all(context, read_deleted = 'no',get_hidden = get_hidden)
         for persisted_setting in persisted_setting_objs:
             persisted_settings[persisted_setting.name] = persisted_setting.value
-        for setting, value in default_settings.iteritems():
+        for setting, value in copy_settings.iteritems():
             if setting not in persisted_settings:
                 persisted_settings[setting] = value
         return persisted_settings
             
     except Exception as ex:
         LOG.exception(ex)
-        return default_settings
+        return copy_settings
 
 def set_settings(context, new_settings):                           
     """set settings"""
