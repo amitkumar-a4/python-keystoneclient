5a83afabaef3c08f6f47fe89595f25cb822161c0
jignasha-vithalani <jignasha.vithalani@triliodata.com>
2017-04-01 11:08:32 +0000

    TVAULT-1614 #comment add context

--- workloadmgr/api/v1/workloads.py
+++ workloadmgr/api/v1/workloads.py
@@ -797,13 +797,13 @@ class WorkloadMgrsController(wsgi.Controller):
             html = '<html><head></head><body>'
             html += 'Test email</body></html>'
             try:
-                 settings = settings_module.get_settings() 
+                 settings = settings_module.get_settings(context) 
                  import re
                  for setting in settings:
                      if setting.strip().find('smtp_') >= 0:
-                        value = settings_module.get_settings().get(setting)
+                        value = settings_module.get_settings(context).get(setting)
                         if (value == "" or len(value) <= 1) and setting != 'smtp_email_enable':
-                           if settings_module.get_settings().get('smtp_server_name') == 'localhost' and (setting == 'smtp_server_password' or setting == 'smtp_server_username'):
+                           if settings_module.get_settings(context).get('smtp_server_name') == 'localhost' and (setting == 'smtp_server_password' or setting == 'smtp_server_username'):
                               continue
                            else:
                                  raise exception.ErrorOccurred("Mandatory field "+setting+" cannot be empty") 
@@ -813,23 +813,23 @@ class WorkloadMgrsController(wsgi.Controller):
                              raise exception.ErrorOccurred(setting+" cannot be greater than 10") 
 
                  msg = MIMEMultipart('alternative')
-                 msg['From'] = settings_module.get_settings().get('smtp_default_sender')
-                 if settings_module.get_settings().get('smtp_default_recipient') is None: 
+                 msg['From'] = settings_module.get_settings(context).get('smtp_default_sender')
+                 if settings_module.get_settings(context).get('smtp_default_recipient') is None: 
                     msg['To'] = msg['From']
                  else:
-                      msg['To'] =  settings_module.get_settings().get('smtp_default_recipient')
+                      msg['To'] =  settings_module.get_settings(context).get('smtp_default_recipient')
  
                  msg['Subject'] = 'Testing email configuration'
                  part2 = MIMEText(html, 'html')
                  msg.attach(part2)
                  try:
-                     socket.setdefaulttimeout(int(settings_module.get_settings().get('smtp_timeout')))
-                     s = smtplib.SMTP(settings_module.get_settings().get('smtp_server_name'),int(settings_module.get_settings().get('smtp_port')))
-                     if settings_module.get_settings().get('smtp_server_name') != 'localhost':
+                     socket.setdefaulttimeout(int(settings_module.get_settings(context).get('smtp_timeout')))
+                     s = smtplib.SMTP(settings_module.get_settings(context).get('smtp_server_name'),int(settings_module.get_settings(context).get('smtp_port')))
+                     if settings_module.get_settings(context).get('smtp_server_name') != 'localhost':
                         s.ehlo()
                         s.starttls()
                         s.ehlo
-                        s.login(settings_module.get_settings().get('smtp_server_username'),settings_module.get_settings().get('smtp_server_password'))
+                        s.login(settings_module.get_settings(context).get('smtp_server_username'),settings_module.get_settings(context).get('smtp_server_password'))
                      s.sendmail(msg['From'], msg['To'], msg.as_string())
                      s.quit()
                  except smtplib.SMTPException as ex:
