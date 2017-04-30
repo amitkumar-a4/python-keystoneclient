d29794a135cacf497baee2fe3e7b7b250f4bd9d4
TrilioBuild <trilio.build@gmail.com>
2017-04-30 06:58:06 +0530

    Updating version to 2.3.60

--- workloadmgr/db/sqlalchemy/models.py
+++ workloadmgr/db/sqlalchemy/models.py
@@ -24,7 +24,7 @@ from workloadmgr.vault import vault
 FLAGS = flags.FLAGS
 BASE = declarative_base()
 
-DB_VERSION = '2.3.59'
+DB_VERSION = '2.3.60'
 
 class WorkloadsBase(object):
     """Base class for Workloads Models."""
