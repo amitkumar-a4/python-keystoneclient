908a0c6e3ed1ff592a1f957dc27ea0fb0b4f00aa
jignasha-vithalani <jignasha.vithalani@triliodata.com>
2017-06-05 00:59:33 +0000

    TVAULT-1705

--- workloadmgr/db/sqlalchemy/api.py
+++ workloadmgr/db/sqlalchemy/api.py
@@ -1089,8 +1089,9 @@ def snapshot_get_running_snapshots_by_host(context, **kwargs):
 def snapshot_get_all(context, **kwargs):
     qs = model_query(context, models.Snapshots, **kwargs).\
                     options(sa_orm.joinedload(models.Snapshots.metadata))
+    #qs = qs.join(models.SnapshotVMs, models.Snapshots.id == models.SnapshotVMs.snapshot_id)
     if 'workload_id' in kwargs and kwargs['workload_id'] is not None and kwargs['workload_id'] != '':  
-       qs = qs.filter_by(workload_id=kwargs['workload_id'])
+       qs = qs.filter(models.Snapshots.workload_id == kwargs['workload_id'])
     if 'host' in kwargs and kwargs['host'] is not None and kwargs['host'] != '':
        qs = qs.filter(models.Snapshots.host == kwargs['host'])
     if 'date_from' in kwargs and kwargs['date_from'] is not None and kwargs['date_from'] != '':
@@ -1102,18 +1103,30 @@ def snapshot_get_all(context, **kwargs):
                       models.Snapshots.created_at <= func.date_format(date_to,'%y-%m-%dT%H:%i:%s')))
 
     if not is_admin_context(context):
-       qs = qs.filter_by(project_id=context.project_id)
+       qs = qs.filter(models.Snapshots.project_id == context.project_id)
     else:
          if 'get_all' in kwargs and kwargs['get_all'] is not True:
-            qs = qs.filter_by(project_id=context.project_id)
+            qs = qs.filter(models.Snapshots.project_id == context.project_id)
     if 'status' in kwargs and kwargs['status'] is not None:
-       qs = qs.filter_by(status=kwargs['status'])
+       qs = qs.filter(models.Snapshots.status == kwargs['status'])
 
     qs = qs.order_by(models.Snapshots.created_at.desc())
     if 'end' in kwargs and kwargs['end'] != 0:
        qs = qs.limit(kwargs['end'])
     if 'start' in kwargs and kwargs['start'] != 0:
        qs = qs.offset(kwargs['start'])
+
+    if 'get_instances' in kwargs and kwargs['get_instances'] is True:
+       snapshots = qs.all()
+       i = 0
+       for snapshot in snapshots:
+           instances = []
+           snapshot_vms = snapshot_vms_get(context, snapshot.id)
+           for snapshot_vm in snapshot_vms:
+               instances.append(snapshot_vm)
+           snapshots[i].instances = instances
+           i = i + 1
+       return snapshots
     return qs.all() 
 
 @require_context                            
